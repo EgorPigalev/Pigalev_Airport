776d5e4c6b0bbe682701d84f33055e3110e56f07
Егор Пигалёв <1pigaleve@mail.ru>
2022-11-03 14:49:36 +0300

    Доработки

--- Airport/Pages/AddTickets.xaml.cs
+++ Airport/Pages/AddTickets.xaml.cs
@@ -20,9 +20,38 @@ namespace Airport.Pages
     /// </summary>
     public partial class AddTickets : Page
     {
+        Box_Offic ticket;
+        bool flagUpdate = false;
+        public void uploadFields()  // метод для заполнения списков
+        {
+            cbDeparture_point.ItemsSource = Base.BE.Citys.ToList();
+            cbDeparture_point.SelectedValuePath = "id_city";
+            cbDeparture_point.DisplayMemberPath = "city";
+
+            cbArrival_point.ItemsSource = Base.BE.Citys.ToList();
+            cbArrival_point.SelectedValuePath = "id_city";
+            cbArrival_point.DisplayMemberPath = "city";
+
+            cbPassenger.ItemsSource = Base.BE.Passengers.ToList();
+            cbPassenger.SelectedValuePath = "id_passenger";
+            cbPassenger.DisplayMemberPath = "surname";
+
+            cbEmployee.ItemsSource = Base.BE.Employees.ToList();
+            cbEmployee.SelectedValuePath = "id_employee";
+            cbEmployee.DisplayMemberPath = "surname";
+
+            lbDiscount.ItemsSource = Base.BE.Discounts.ToList();
+        }
         public AddTickets()
         {
             InitializeComponent();
+            uploadFields();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Frameclass.MainFrame.Navigate(new ListOfTickets());
         }
     }
+    
 }
